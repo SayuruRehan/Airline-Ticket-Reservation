55deda32e3ab244facc12316b97eb063aeaff405
Sayuru Rehan <42121050+SayuruRehan@users.noreply.github.com>
2022-06-12 11:14:14 +0530

    Update pay.js

--- js/pay.js
+++ js/pay.js
@@ -2,18 +2,15 @@ var cardnumber = document.getElementById('Crdnumber')
 //const cvv = document.getElementById('cvv')
 function success()
 {
-form.addEventListener('submit',(e) =>{
+  form.addEventListener('submit',(e) =>{
  
-if(cardnumber.value.length > 10 || cardnumber.value.length <10)
-{
-    alert("card number must be 10 characters");
+   if(cardnumber.value.length > 10 || cardnumber.value.length <10)
+   {
+       alert("card number must be 10 characters");
+   }
+    else
+    {
+       alert("Payment successful");
+    }
+  }
 }
- else
- {
-    alert("Payment successful");
- }
-}
-)
-}
-
-
